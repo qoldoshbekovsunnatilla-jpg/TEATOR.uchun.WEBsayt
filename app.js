6cd4569f77b1203816624dfb562fc0f828e413b2
SUNNATILLA <qoldoshbekovsunnat@gmail.com>
2025-11-09 23:59:59 +0800

    tugadi

---
+++ app.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  document.getElementById('reklamaBox').style.display = 'block';
+}, 3000);
+
+function yopReklama() {
+  document.getElementById('reklamaBox').style.display = 'none';
+}
